f3ce9ea41ead77b72d48adef4c43d9cc8e8308fd
DanielBachler <daniel.bachler@comcast.net>
2019-11-14 12:08:47 -0700

    Web scraping for first site is done
    Completed the webscraping for the colt site, and now ouputs to unique txt file.  Fields are seperate with '|' as it is a unique character not present in any of the fields.

--- webScraper.py
+++ webScraper.py
@@ -5,18 +5,73 @@
 website1 = "https://dst.lbl.gov/ACSSoftware/colt/api/cern/jet/stat/Descriptive.html"
 
 # Import
-import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 import urllib.request
-import time
+import re
 
 # Get web info for first site
 response = requests.get(website1)
 
+# Function for parsing information from tr tag for first database
+def parseTRtag(tag):
+    # List of attributes in order of; name, return type, input types, desc text
+    # Get td tags from tr
+    tds = tag.find_all('td')
+    # Get text from first td tag
+    returnType = tds[0].getText()
+    returnType = returnType.split("\xa0")[1]
+
+    # Separate second table entry into fields
+    desc = tds[1].getText()
+    desc = re.sub("\n|\t", "", desc)
+    separateNameandDesc = desc.split(")", 1)
+    # Get function name
+    separateName = separateNameandDesc[0].split("(")
+    name = separateName[0]
+
+    # Get input types
+    inputTypes = separateName[1]
+    inputTypes = inputTypes.split(",")
+    for i in range(0,len(inputTypes)):
+        temp = inputTypes[i].strip()
+        temp = re.sub("\xa0", " ", temp)
+        inputTypes[i] = temp.split(" ")[0]
+    inputTypes = ",".join(inputTypes)
+
+    # Get description text
+    text = separateNameandDesc[1]
+    text = text.strip()
+
+    # Return values
+    return([name, returnType, inputTypes, text])
+
+
 # Parse response
+# Create soup object
 soup = BeautifulSoup(response.text, "html.parser")
-print(soup.prettify())
-# for tr in soup.find_all('tr'):
-#     if tr["bgcolor"] == "white":
-#         print(tr)
+# Get the 3rd table, has all information inside
+table = soup.find_all('table')[2]
+# Find all the tr tags
+tr = table.find_all("tr")
+
+# List of attributes for each entry
+attributes = []
+
+# Loop through all tr tags
+for tri in tr:
+    # Get color of current tr tag
+    bgcolor = tri['bgcolor']
+    # If white parse
+    if bgcolor == 'white':
+        attributes.append(parseTRtag(tri))
+
+# for i in range(0,5):
+#     print(attributes[i])
+
+# Print to a file
+outputFile = open("coltAttributes.txt", "w")
+for attribute in attributes:
+    outputFile.write("|".join(attribute))
+    outputFile.write("\n")
+outputFile.close()
