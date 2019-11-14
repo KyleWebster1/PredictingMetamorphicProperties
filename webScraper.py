5b7b6d4703b2ae2589e2482622e021fa20f8587b
Dan <daniel.bachler@comcast.net>
2019-11-14 09:31:13 -0700

    Create webScraper.py
    Added the beginning of the webscraper to gather data from sites

---
+++ webScraper.py
@@ -0,0 +1,5 @@
+# Scrape the first webpage
+# "https://dst.lbl.gov/ACSSoftware/colt/api/cern/jet/stat/Descriptive.html"
+
+website1 = "https://dst.lbl.gov/ACSSoftware/colt/api/cern/jet/stat/Descriptive.html"
+
